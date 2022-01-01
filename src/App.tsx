f758f1bf7910244d3dea321bec91d6f78b68b986
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-01-01 18:33:12 -0300

    definindo estilos globais e fontes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <h2>Hello world</h2>
+    <>
+      <h2>Hello world</h2>
+      <GlobalStyle />
+    </>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --red: #E62E4D;
+    --green: #33CC95;
+    --blue: #5429CC;
+    --blue-light: #9933FF;
+    --shape: #FFFFFF;
+    --background: #F0F2F5;
+    --text: #969CB3;
+    --title: #363F5F;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; // 14px
+    }
+  }
+
+  body {
+    background-color: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  strong, h1, h2, h3, h4, h5, h6 {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
