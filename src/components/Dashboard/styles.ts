19bfcc53a112f0008504c2862185e281dbef3d30
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-01-02 02:24:50 -0300

    criando componente dashboard e summary

---
+++ src/components/Dashboard/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 2.5rem 1rem;
+`
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,34 @@
+import incomeImg from '../../assets/income.svg'
+import outComeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Icone de entradas" />
+        </header>
+        <strong>R$ 1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outComeImg} alt="Icone de saídas" />
+        </header>
+        <strong>R$ 500,00</strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Icone de sifrão simbolizando o total" />
+        </header>
+        <strong>R$ 500,00</strong>
+      </div>
+    </Container>
+  )
+}
